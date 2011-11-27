1fa544b51733ca07c1aeb8e1002228dc6542c520
Dermot Brennan <dermot.brennan@gmail.com>
2011-11-27 17:52:32 +0000

    Added procfile and migration

--- README.md
+++ README.md
@@ -4,12 +4,13 @@ A very basic URL shortener written in Clojure and Noir.
 
 ## Usage
 
+```bash
 lein deps
 source bin/set_database_url.sh # set DATABASE_URL parameter
 lein run
+```
 
 Go to http://10.0.0.3:8080/
-
 
 ## License
 
--- src/shorturl/models/migration.clj
+++ src/shorturl/models/migration.clj
@@ -9,7 +9,16 @@
                       [:shortened :varchar "NULL"]
                       [:original :varchar "NOT NULL"]
                       [:views :integer "NOT NULL"]
-                      [:created_at :timestamp "NOT NULL" "DEFAULT CURRENT_TIMESTAMP"])))
+                      [:created_at :timestamp "NOT NULL" "DEFAULT CURRENT_TIMESTAMP"]
+                      ["CONSTRAINT urls_pkey PRIMARY KEY (id)"] ,
+                      ["CONSTRAINT urls_original_key UNIQUE (original)"],
+                      ["CONSTRAINT urls_shortened_key UNIQUE (shortened)"]
+                      )
+    (sql/do-commands "CREATE INDEX urls_created_at_idx ON urls USING btree (created_at)"
+                     "CREATE INDEX urls_id_idx ON urls USING btree (id)"
+                     "CREATE INDEX urls_original_idx ON urls USING btree (original)"
+                     "CREATE INDEX urls_shortened_idx ON urls USING btree (shortened)"
+                     "CREATE INDEX urls_views_idx ON urls USING btree (views)")))
 
 (defn -main []
   (print "Migrating database...") (flush)
